5f26c0c68a22ce27c74992747ebf4d43ba9b39bf
agustin <aguszorza@gmail.com>
2020-03-23 21:29:49 +0100

    register form

--- Exercice_2/with_faults/app/routes.py
+++ Exercice_2/with_faults/app/routes.py
@@ -1,6 +1,10 @@
 from app import app, db
 from app.models import User
-from flask import request
+from flask import request, redirect
+
+REGISTER_ERROR = {'in_use': 'The username or the email is already in use',
+                  'empty_fields': 'You must fill all the fields'
+                  }
 
 
 @app.route('/')
@@ -9,16 +13,25 @@ def index():
     return "Hello, World!"
 
 
+@app.route('/register', methods=["GET"])
+def get_register_form():
+    response = open("./templates/register.html").read()%("", "", "")
+    return response
+
+
 @app.route('/register', methods=["POST"])
 def register():
-    username = request.args.get('username', None)
-    email = request.args.get('email', None)
-    password = request.args.get('password', None)
-    user = User(username=username, email=email)
-    user.set_password(password)
-    db.session.add(user)
-    db.session.commit()
-    return "", 204
+    username = request.form.get('username', "")
+    email = request.form.get('email', "")
+    password = request.form.get('password', None)
+    error_message = REGISTER_ERROR['empty_fields']
+    if username and email and password:
+        saved = save_user(email, username, password)
+        error_message = REGISTER_ERROR['in_use']
+        if saved:
+            return redirect('/login')
+    response = open("./templates/register.html").read()%(email, username, error_message)
+    return response, 404
 
 
 @app.route('/login')
@@ -27,7 +40,7 @@ def login():
     password = request.args.get('password', None)
     if not username:
         response = open("./templates/login.html").read()%("")
-        return response, 404
+        return response
     elif not password:
         response = open("./templates/login.html").read()%(username)
         return response, 404
@@ -35,7 +48,7 @@ def login():
         # TODO: add session
         return "ok", 200
     response = open("./templates/login.html").read()%(username)
-    return response
+    return response, 404
 
 
 def check_user(username, password):
@@ -43,3 +56,14 @@ def check_user(username, password):
     if result and password:
         return result.check_password(password)
     return False
+
+
+def save_user(email, username, password):
+    try:
+        user = User(username=username, email=email)
+        user.set_password(password)
+        db.session.add(user)
+        db.session.commit()
+        return True
+    except Exception:
+        return False

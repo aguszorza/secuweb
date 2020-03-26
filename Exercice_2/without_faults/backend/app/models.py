04fc5ae89d830d1cee68762af0431201c4e1f7c2
achraftha <62549204+achraftha@users.noreply.github.com>
2020-03-26 17:22:45 +0100

    Update models.py

--- Exercice_2/without_faults/backend/app/models.py
+++ Exercice_2/without_faults/backend/app/models.py
@@ -1,20 +1,9 @@
-from werkzeug.security import generate_password_hash, check_password_hash
-
-# ...
 from app import db
 from flask_login import UserMixin
 from app import login
-class User(db.Model):
-    # ...
+from werkzeug.security import generate_password_hash, check_password_hash
 
-    def set_password(self, password):
-        self.password_hash = generate_password_hash(password)
 
-    def check_password(self, password):
-        if password:
-            return check_password_hash(self.password_hash, password)
-        else :
-            return false
 
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -24,7 +13,13 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return '<User {}>'.format(self.username)
+    def set_password(self, password):
+        self.password_hash = generate_password_hash(password)
 
+    def check_password(self, password):
+        if password:
+            return check_password_hash(self.password_hash, password)
+        return false
 
 @login.user_loader
 def load_user(user_id):
